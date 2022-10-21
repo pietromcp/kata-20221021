0b172a451aa45db2fd685b70bbfd112a4a7cc02b
Pietro Martinelli <pietro.martinelli@codiceplastico.com>
2022-10-21 17:09:40 +0200

    Rework intToRoman using a different data structure

--- roman.js
+++ roman.js
@@ -1,16 +1,21 @@
-const singleCharInts = {
-  1: 'I',
-  5: 'V',
-  10: 'X',
-  50: 'L',
-  100: 'C',
-  500: 'D',
-  1000: 'M',
-  1666: 'MDCLXVI'
-}
+const singleCharInts = [
+  { key: 1666, value: "MDCLXVI" },
+  { key: 1000, value: "M" },
+  { key: 500, value: "D" },
+  { key: 100, value: "C" },
+  { key: 50, value: "L" },
+  { key: 10, value: "X" },
+  { key: 5, value: "V" },
+  { key: 1, value: "I" }
+]
 
 function intToRoman(n) {
-  return singleCharInts[n]
+  return singleCharInts.find(x => x.key === n).value
+  // let output = ""
+
+
+
+  // return output || singleCharInts[n]
 }
 
 module.exports = {
