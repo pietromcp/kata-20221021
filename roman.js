00600f0213e173fb6189d77fa98ccc3c6284c379
Daniele Gargiulo <daniele.gargiulo@codiceplastico.com>
2022-10-21 16:47:17 +0200

    convert 10 to X

--- roman.js
+++ roman.js
@@ -1,12 +1,13 @@
 const singleCharInts = {
-	1: 'I',
-	5: 'V'
+  1: 'I',
+  5: 'V',
+  10: 'X',
 }
 
 function intToRoman(n) {
-	return singleCharInts[n]
+  return singleCharInts[n]
 }
 
 module.exports = {
-	intToRoman
+  intToRoman
 }
--- spec/int2roman.spec.js
+++ spec/int2roman.spec.js
@@ -1,11 +1,15 @@
 const intToRoman = require("../roman.js").intToRoman
 
-describe("Convert int into single-char roman", function() {
-    it("1 --> I", function() {
-        expect(intToRoman(1)).toBe("I")
-    })
+describe("Convert int into single-char roman", function () {
+  it("1 --> I", function () {
+    expect(intToRoman(1)).toBe("I")
+  })
 
-    it("5 --> V", function() {
-        expect(intToRoman(5)).toBe("V")
-    })
+  it("5 --> V", function () {
+    expect(intToRoman(5)).toBe("V")
+  })
+
+  it("10 --> X", function () {
+    expect(intToRoman(10)).toBe("X")
+  })
 });
